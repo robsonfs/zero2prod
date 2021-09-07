013dcdf619028401c41f314e26e16219817ef1e2
Robson Santos <robson.santos@solcap.com.br>
2021-09-06 21:08:54 -0300

    Adding a health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prod::run;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let host = TcpListener::bind("127.0.0.1:8001").unwrap();
+    run(host)?.await
 }
